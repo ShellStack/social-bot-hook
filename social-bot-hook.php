b1d582998216c0420d3e2f70938619ca05d98df4
Shahid Shaikh <UnixRoot@Shahids-MacBook-Air.local>
2016-10-03 00:40:28 +0530

    wp modification

--- social-bot-hook.php
+++ social-bot-hook.php
@@ -3,20 +3,20 @@
 Plugin Name: Social bot hook.
 Description: This plugin acts as hook for social bot SaaS Service.
 */
-function getAllPost() {
-  $args = array('posts_per_page' => 1,'orderby' => 'rand');
-  $rand_posts = get_posts($args);
-  if (empty($rand_posts)) {
+function sbh_retrieve_random_post() {
+  $sbhArguement = array('posts_per_page' => 1,'orderby' => 'rand');
+  $sbhRandomPost = get_posts($sbhArguement);
+  if (empty($sbhRandomPost)) {
         return null;
   }
-  return $rand_posts;
+  return $sbhRandomPost;
 }
 add_filter('allowed_http_origins','add_allowed_origins');
-add_action('wp_head', 'getAllPost');
+add_action('wp_head', 'sbh_retrieve_random_post');
 add_action('rest_api_init', function(){
         register_rest_route( 'socialbot/v1', '/posts/', array(
                 'methods' => 'GET',
-                'callback' => 'getAllPost',
+                'callback' => 'sbh_retrieve_random_post',
         ));
 });
 ?>
